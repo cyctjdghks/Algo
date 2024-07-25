6279e656d201b3749d52c105a147621112643c3a
박성환 <71548751+cyctjdghks@users.noreply.github.com>
2024-07-25 11:10:43 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 156 ms, Memory: 25940 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -1,79 +1,81 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    // 걷는다면 1초 후에 X-1 또는 X+1로 이동
-    // 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동
-    // 첫째 줄에 수빈이가 동생을 찾는 가장 빠른 시간을 출력한다.
-    // 둘째 줄에는 가장 빠른 시간으로 수빈이가 동생을 찾는 방법의 수를 출력한다.
+
     static int N, K;
-    static int[] time = new int[100001];
-    static int resTime = 9999_9999;
-    static int resCount = 0;
+    static int[] arr;
+    static int res, cnt;
 
     public static void main(String[] args) {
+        // N => K
+
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
         K = sc.nextInt();
 
-        if (N >= K) { // 동생이 뒤에 있으면 뒤로만 가야됨
-            System.out.println(N - K);
-            System.out.println(1);
-            return;
-        }
-        
+        arr = new int[100001];
+        Arrays.fill(arr, Integer.MAX_VALUE);
+
+        res = Integer.MAX_VALUE;
+        cnt = 0;
+
         bfs();
 
-        System.out.println(resTime);
-        System.out.println(resCount);
+        System.out.println(res + " " + cnt);
     }
 
     private static void bfs() {
-        Queue<Integer> q = new LinkedList<Integer>();
+        Queue<Node> q = new LinkedList<>();
 
-        q.offer(N);
-        time[N] = 1;
+        q.offer(new Node(N, 0));
+        arr[N] = 0;
 
         while (!q.isEmpty()) {
-            int now = q.poll();
+            Node node = q.poll();
 
-            // now 방문 시간이 최소 시간보다 크면 뒤는 더 볼 필요 없음
-            if (resTime < time[now]) return;
+            int index = node.index;
+            int count = node.count;
 
-            for (int i = 0; i < 3; i++) {
-                int next;
+            if (index == K && res >= count) {
+                cnt++;
+                res = Math.min(res, count);
+                continue;
+            }
 
-                if (i == 0) next = now + 1;
-                else if (i == 1) next = now - 1;
-                else next = now * 2;
+            if (count > res) {
+                continue;
+            }
 
-                if (next < 0 || next > 100000) continue;
-
-                if (next == K) {
-                    resTime = time[now];
-                    resCount++;
-                }
-
-                // 첫 방문이거나 (time[next] == 0)
-                // 이미 방문한 곳이어도 같은 시간에 방문했다면 (time[next] == time[now] + 1)
-                // 경우의 수에 추가될 수 있기 때문에 Queue 에 한번 더 넣어줌
-                if (time[next] == 0 || time[next] == time[now] + 1) {
-                    q.offer(next);
-                    time[next] = time[now] + 1;
-                }
+            if (isInBound(index + 1) && arr[index + 1] >= count) {
+                q.offer(new Node(index + 1, count + 1));
+                arr[index + 1] = count;
+            }
+            if (isInBound(index - 1) && arr[index - 1] >= count) {
+                q.offer(new Node(index - 1, count + 1));
+                arr[index - 1] = count;
+            }
+            if (isInBound(index * 2) && arr[index * 2] >= count) {
+                q.offer(new Node(index * 2, count + 1));
+                arr[index * 2] = count;
             }
         }
     }
 
-    public static class Data {
-        int index; // 현재 위치
-        int time; // 지난 시간
+    static boolean isInBound(int x) {
+        return x >= 0 && x <= 100000;
+    }
 
-        public Data(int index, int time) {
+    static class Node {
+        int index;
+        int count;
+
+        public Node(int index, int count) {
             this.index = index;
-            this.time = time;
+            this.count = count;
         }
     }
 }
